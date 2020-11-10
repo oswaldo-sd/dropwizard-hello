e318c40749d1429846b6ee92fec48882c6cb69a0
Oswaldo Sanchez D <oswaldo.sanchez@dreambox.com>
2020-11-10 01:08:51 -0600

    Date format

--- src/main/java/hello/world/DropwizardHelloWorldApplication.java
+++ src/main/java/hello/world/DropwizardHelloWorldApplication.java
@@ -6,6 +6,9 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class DropwizardHelloWorldApplication extends Application<DropwizardHelloWorldConfiguration> {
 
     public static void main(final String[] args) throws Exception {
@@ -33,6 +36,9 @@ public class DropwizardHelloWorldApplication extends Application<DropwizardHello
 
         final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
         environment.healthChecks().register("template", healthCheck);
+
+        DateFormat eventDateFormat = new SimpleDateFormat(configuration.getDateFormat());
+        environment.getObjectMapper().setDateFormat(eventDateFormat);
     }
 
 }
--- src/main/java/hello/world/DropwizardHelloWorldConfiguration.java
+++ src/main/java/hello/world/DropwizardHelloWorldConfiguration.java
@@ -12,6 +12,9 @@ public class DropwizardHelloWorldConfiguration extends Configuration {
     @NotEmpty
     private String defaultName = "Stranger";
 
+    @NotEmpty
+    private String dateFormat;
+
     @JsonProperty
     public String getTemplate() {
         return template;
@@ -30,5 +33,9 @@ public class DropwizardHelloWorldConfiguration extends Configuration {
     @JsonProperty
     public void setDefaultName(String name) {
         this.defaultName = name;
+    }
+
+    public String getDateFormat() {
+        return dateFormat;
     }
 }
